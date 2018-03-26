3026494bcdb42a58b4183d975bd087bd9e8e89b8
Hichem DRISS <hichdriss@gmail.com>
2018-03-26 15:46:47 +0200

    generate first component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { CoreModule } from './core/core.module';
+import { HomeModule } from './home/home.module';
+import { PageError404Module } from './page-error404/page-error404.module';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    HomeModule,
+    PageError404Module
   ],
   declarations: [
     AppComponent
