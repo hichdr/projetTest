db3e587fb66708f63092e2f60d16ddea723322d8
Hichem DRISS <hichdriss@gmail.com>
2018-03-28 09:55:48 +0200

    Rajouter FormBuilder, FormGroup

--- my-app/src/app/shared/components/form/form.component.ts
+++ my-app/src/app/shared/components/form/form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnChanges, Output, EventEmitter } from '@angular/core';
 import { State } from '../../enums/state.enum';
 import { Item } from '../../models/item.model';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 
 @Component({
@@ -9,26 +10,30 @@ import { Item } from '../../models/item.model';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit, OnChanges {
+  form: FormGroup;
   libelles = Object.values(State);
-  newItem: Item;
   @Output() nItem: EventEmitter<Item> = new EventEmitter();
-  constructor() { }
 
-  ngOnInit() {
-    this.newItem = {
-      id: '',
-      name: '',
+  constructor(private fb: FormBuilder) {}
+
+  createForm() {
+    this.form = this.fb.group({
+      name: '', // <--- the FormControl called "name"
       reference: '',
       state: State.ALIVRER
-    };
+    });
+  }
+
+  ngOnInit() {
+    this.createForm();
   }
 
   ngOnChanges() {
-    console.log(this.newItem.name);
+    console.log();
   }
 
   process(): void {
-    this.nItem.emit(this.newItem);
-    console.log(this.newItem);
+    this.nItem.emit();
+    console.log();
   }
 }
