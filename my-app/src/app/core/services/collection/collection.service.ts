92d82aafebc77be19cdbbc388962de8c8bd5a4cf
Hichem DRISS <hichdriss@gmail.com>
2018-03-28 15:19:52 +0200

    HttpClient

--- my-app/src/app/core/services/collection/collection.service.ts
+++ my-app/src/app/core/services/collection/collection.service.ts
@@ -3,28 +3,31 @@ import { Item } from '../../../shared/models/item.model';
 import { COLLECTION } from '../collection';
 import { Observable } from 'rxjs/Observable';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+// import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class CollectionService {
   itemsCollection: AngularFirestoreCollection<Item>;
   // private _collection: Item[];
-  private _collection: Observable<Item[]>;
+  private _collection$: Observable<Item[]>;
 
   constructor(
-    private afs: AngularFirestore
+    private afs: AngularFirestore,
+    // private http: HttpClient
   ) {
     this.itemsCollection = afs.collection<Item>('collection');
-    this._collection = afs.itemsCollection.valueChanges();
+    this._collection$ = afs.itemsCollection.valueChanges();
+    // this._collection$ = this.http.get('mon_url_api_rest/collection');
   }
 
   // get collection de type script
   get collection$(): Observable<Item[]> {
-    return this._collection;
+    return this._collection$;
   }
 
   // set collection de type script
   set collection$(collection: Observable<Item[]>) {
-    this._collection = collection;
+    this._collection$ = collection;
   }
 
   // add item
--- my-app/src/app/items/containers/list-items/list-items.component.ts
+++ my-app/src/app/items/containers/list-items/list-items.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Item } from '../../../shared/models/item.model';
 import { CollectionService } from '../../../core/services/collection/collection.service';
 import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-list-items',
@@ -9,20 +10,21 @@ import { Subscription } from 'rxjs/Subscription';
   styleUrls: ['./list-items.component.css']
 })
 export class ListItemsComponent implements OnInit, OnDestroy {
-  sub: Subscription;
-  collection: Item[];
+  // sub: Subscription;
+  collection$: Observable<Item[]>;
   constructor(
     private collectionService: CollectionService
   ) {}
 
   ngOnInit() {
-    this.sub = this.collectionService.collection$.subscribe((data)) => {
-      this.collection = data;
-    }
+    // this.sub = this.collectionService.collection$.subscribe((data)) => {
+    //   this.collection = data;
+    // }
+    this.collection$ = this.collectionService.collection$;
   }
 
   ngOnDestroy() {
-    this.sub.unsubscribe();
+    // this.sub.unsubscribe();
   }
 
 }
